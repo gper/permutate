e9983493ca0fa81e0556000ae5ff5a1865659459
cpawling <59662702+cpawling@users.noreply.github.com>
2021-11-21 12:23:55 -0600

    text push
    git push test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Hahah this in the p area is how we edit text for the page
         </p>
         <a
           className="App-link"
