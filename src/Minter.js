838af5b38b7040815b5ef93522566fa7762996f3
cpawling <59662702+cpawling@users.noreply.github.com>
2021-11-23 19:14:17 -0600

    make CSS for Minter
    Making CSS for Minter, and connect

--- src/Minter.js
+++ src/Minter.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { connectWallet, getCurrentWalletConnected, mintNFT } from "./utils/interact.js";
+import '/Minter.css';
 
 const Minter = (props) => {
 
