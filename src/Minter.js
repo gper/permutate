1beb35fc2c0b56654d6cdb00e8e1fdbe13b6ddab
Zlurvey <94583056+Zlurvey@users.noreply.github.com>
2021-11-23 18:33:01 -0600

    Minter

--- src/Minter.js
+++ src/Minter.js
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import {
+  connectWallet,
+  getCurrentWalletConnected //import here
+} from "./Utils/Interact";
 
 const Minter = (props) => {
 
@@ -8,13 +12,43 @@ const Minter = (props) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [url, setURL] = useState("");
- 
-  useEffect(async () => { //TODO: implement
-    
-  }, []);
 
-  const connectWalletPressed = async () => { //TODO: implement
-   
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text-field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+ 
+  useEffect(async () => {
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status);
+    addWalletListener();  
+}, []);
+  
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
   };
 
   const onMintPressed = async () => { //TODO: implement
